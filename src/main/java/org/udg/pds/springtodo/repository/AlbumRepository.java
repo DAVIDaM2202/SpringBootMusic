a2536b50084d03846e4ffabfbb85f53f17ee8228
n0ss4 <nghazouani@outlook.com>
2022-04-08 15:36:20 +0200

    Create the repository selects

--- src/main/java/org/udg/pds/springtodo/repository/AlbumRepository.java
+++ src/main/java/org/udg/pds/springtodo/repository/AlbumRepository.java
@@ -1,9 +1,19 @@
 package org.udg.pds.springtodo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.udg.pds.springtodo.entity.Album;
+import org.udg.pds.springtodo.entity.Artista;
+
+import java.util.List;
 
 @Repository
 public interface AlbumRepository extends JpaRepository<Album,Long> {
+    @Query("SELECT a FROM album a where a.artista=:artista")
+    List<Album> buscarAlbumPerArtistes(@Param("artista") Artista artista);
+
+    @Query("SELECT a FROM album a where a.titol=:titol")
+    List<Album> buscarAlbumPerTitol(@Param("titol") String titol);
 }
